c793c3eeca809c85af1fbdbbde6ba7ecc8d1a4e8
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-04-08 02:34:34 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyDS62TKlek4tqTJadD8tHBR4SCTr4D6WWI",
+  authDomain: "jimswd.firebaseapp.com",
+  projectId: "jimswd",
+  databaseURL: "jimswd.appspot.com",
+  storageBucket: "jimswd.appspot.com",
+  messagingSenderId: "524753032159",
+  appId: "1:524753032159:web:fdb7fa5c7dc41eb9e12a6a",
+  measurementId: "G-W58BMRVRNS"
 })
 
 const db = firebaseApp.firestore();
