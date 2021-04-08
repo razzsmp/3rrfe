5f6629c276a6f1d96bbeaa1db22effbf58102cd0
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-04-08 02:43:45 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDS62TKlek4tqTJadD8tHBR4SCTr4D6WWI",
   authDomain: "jimswd.firebaseapp.com",
   projectId: "jimswd",
-  databaseURL: "jimswd.appspot.com",
+  databaseURL: "jimswd-default-rtdb.firebaseio.com",
   storageBucket: "jimswd.appspot.com",
   messagingSenderId: "524753032159",
   appId: "1:524753032159:web:fdb7fa5c7dc41eb9e12a6a",
